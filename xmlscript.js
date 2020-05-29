10d0f2237e1902895d07390c682d00c435ee2548
Dave <dsottimano@gmail.com>
2020-05-29 16:23:28 -0500

    Update xmlscript.js

--- xmlscript.js
+++ xmlscript.js
@@ -8,23 +8,24 @@
 * -------------------------------------------------------------------------*/
 
 /**
-* Returns URLs in sitemap.xml file
+* Returns URLs in sitemap.xml file or sitemap index file
 *
 * @param {"https://www.google.com/gmail/sitemap.xml"} sitemapUrl REQUIRED The url of the sitemap
-* @param {"http://www.sitemaps.org/schemas/sitemap/0.9"} namespace REQUIRED Look at the source of the xml sitemap, look for the xmlns value 
 * @return Returns urls <loc> from an xml sitemap
 * @customfunction
 */
 
-function sitemap(sitemapUrl,namespace) {
-  
+function EXTRACT_SITEMAP(sitemapUrl) {
   try {
+
     var xml = UrlFetchApp.fetch(sitemapUrl).getContentText();
     var document = XmlService.parse(xml);
     var root = document.getRootElement()
+    var namespace = root.getNamespace().getURI()
     var sitemapNameSpace = XmlService.getNamespace(namespace);
     
-    var urls = root.getChildren('url', sitemapNameSpace)
+    let urls = root.getChildren('url',sitemapNameSpace)[0] ? root.getChildren('url', sitemapNameSpace) : root.getChildren('sitemap', sitemapNameSpace);
+     
     var locs = []
     
     for (var i=0;i <urls.length;i++) {
@@ -33,37 +34,7 @@ function sitemap(sitemapUrl,namespace) {
     
     return locs  
   } catch (e) {
-    return e 
-  }
-}
-
-
-/**
-* Returns URLs in sitemap index file
-*
-* @param {"https://www.google.com/sitemap.xml"} sitemapIndexUrl REQUIRED The url of the sitemap
-* @param {"http://www.sitemaps.org/schemas/sitemap/0.9"} namespace REQUIRED Look at the source of the xml sitemap, look for the xmlns value 
-* @return Returns urls <loc> from an xml sitemap
-* @customfunction
-*/
-
-function sitemapIndex(sitemapIndexUrl,namespace) {
-  
-  try {
-    var xml = UrlFetchApp.fetch(sitemapIndexUrl).getContentText();
-    var document = XmlService.parse(xml);
-    var root = document.getRootElement()
-    var sitemapNameSpace = XmlService.getNamespace(namespace);
-    
-    var urls = root.getChildren('sitemap', sitemapNameSpace)
-    var locs = []
-    
-    for (var i=0;i <urls.length;i++) {
-      locs.push(urls[i].getChild('loc', sitemapNameSpace).getText()) 
-    }
-    
-    return locs  
-  } catch (e) {
-    return e 
+    console.log(e)
+    return e.toString() 
   }
 }
